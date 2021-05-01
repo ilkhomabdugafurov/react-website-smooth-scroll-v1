ff596eef24016bcc441ced88dbf6b40ad7cc918f
coderealistics <coderealistics@yandex.com>
2021-05-01 20:47:17 +0500

    yarn add react-router-dom

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Nav, NavbarContainer} from "./NavbarElements";
 
 const Navbar = () => {
     return (
         <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo>dolla</NavLogo>
+                </NavbarContainer>
+            </Nav>
         </>
     );
 };
